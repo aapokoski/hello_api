96235cea7c4d5334ba4d053fb6eec86b1c7088bb
Aapo Koski <aapokoski@users.noreply.github.com>
2017-01-23 14:08:21 +0200

    Minor update

--- api_server.py
+++ api_server.py
@@ -6,21 +6,21 @@ app = Flask(__name__)
 
 @app.route('/readHello')
 def getRequestHello():
-	return "Hi, I got your GET Request!"
+	return "Got a GET Request!"
 
 #POST REQUEST
 @app.route('/createHello', methods = ['POST'])
 def postRequestHello():
-	return "I see you sent a POST message :-)"
+	return "Got a POST message :-)"
 #UPDATE REQUEST
 @app.route('/updateHello', methods = ['PUT'])
 def updateRequestHello():
-	return "Sending Hello on an PUT request!"
+	return "Received a PUT request!"
 
 #DELETE REQUEST
 @app.route('/deleteHello', methods = ['DELETE'])
 def deleteRequestHello():
-	return "Deleting your hard drive.....haha just kidding! I received a DELETE request!"
+	return "Received a DELETE request!"
 
 if __name__ == '__main__':
     app.debug = True
